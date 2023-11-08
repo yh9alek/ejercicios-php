1784c31a7d7e4c201f7c2b6d69c4ea89943cd181
Yohan Alek <yh9dev@gmail.com>
2023-11-08 16:37:19 -0700

    Se añadió un poco de código al ejercicio 16

--- ejercicio_16.php
+++ ejercicio_16.php
@@ -1,4 +1,10 @@
 <?php
     $numeroAleatorio = rand(1, 10);
     echo $numeroAleatorio;
+
+    $nombre = 'Yohan';
+    $minus = strtolower($nombre);
+
+    $hoy = date('y - m - d');
+    echo $hoy;
 ?>
