9d5635c0ae3f0fcb6d1c93f3f1e42ec3632be870
Yohan Alek <yh9dev@gmail.com>
2023-11-08 12:11:14 -0700

    Se realizó el ejercicio 5 con PHP (Tipos de datos primitivos)

---
+++ ejercicio5.php
@@ -0,0 +1,7 @@
+<?php
+    $nombre = 'Yohan';         // string
+    $edad = 20;                // int
+    $estatura = 1.75;          // float
+    $tieneNovia = false;       // bool o boolean
+    $historialCrimenes = null; // null
+?>
